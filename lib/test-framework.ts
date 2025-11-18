601ba47b2de0ead9cd15d26c5556e2fafa0ae9d8
janlau2001 <janlaurenceolarte02@gmail.com>
2025-11-18 17:12:05 +0800

    fix: completely remove unused TestQuestion import

--- lib/test-framework.ts
+++ lib/test-framework.ts
@@ -5,7 +5,6 @@
 
 import { processQuery } from './query-processor';
 import { testQuestions, getTestSummary } from './test-questions';
-import type { TestQuestion } from './test-questions';
 
 export interface TestResult {
   questionId: number;
